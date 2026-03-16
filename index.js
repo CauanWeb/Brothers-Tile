144938010d0ad080c33015160347457369ed7719
CauanWeb <156857232+idk707@users.noreply.github.com>
2026-03-16 16:55:32 +0000

    Work In Progress

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const slide_menu = document.querySelector(".slide_menu");
 const menu = document.querySelector(".menu");
 
 menu.onclick = () =>{
-    slide_menu.classList.add("active");
+    slide_menu.classList.toggle("active");
+    menu.classList.toggle("active")
 }
