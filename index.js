367fbd4a33b1b11c868cebb71cb049a9b63ad000
CauanWeb <156857232+idk707@users.noreply.github.com>
2026-03-19 15:45:26 +0000

    Work In Progress

--- index.js
+++
@@ -1,7 +0,0 @@
-// const slide_menu = document.querySelector(".slide_menu");
-// const menu = document.querySelector(".menu");
-
-// menu.onclick = () =>{
-//     slide_menu.classList.toggle("active");
-//     menu.classList.toggle("active")
-// }
