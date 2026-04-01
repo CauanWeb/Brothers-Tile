9ab4d51d7243841bb79c0e8a6c28352df220a167
CauanWebOwner <cauanjorge16@gmail.com>
2026-04-01 09:30:29 -0400

    Work In Progress

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'https://unpkg.com/lenis?module'
 
 const lenis = new Lenis({
   duration: 1.2,
-  smooth: true
+  smoothWheel: true
 });
 
 function raf(time) {
@@ -14,12 +14,25 @@ requestAnimationFrame(raf);
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
+    console.log(entry.isIntersecting);
+
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
       observer.unobserve(entry.target);
     }
   });
 }, {
-  threshold: 0.5,
-  rootMargin: "0px 0px -25% 0px"
+  threshold: 0.25,
+  rootMargin: "0px 0px 0px 0px"
+});
+
+document.querySelectorAll('.hidden').forEach((el) => {
+  observer.observe(el);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    lenis.scrollTo(anchor.getAttribute('href'));
+  });
 });
